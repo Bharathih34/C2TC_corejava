77ab5b3199b46aa3c2f1d8ae1311bcc6c48190a3
myusername <bharathinayak34@gmail.com>
2022-10-18 11:47:18 +0530

    first message

--- br/src/br/Br.java
+++ br/src/br/Br.java
@@ -1,10 +1,25 @@
 package br;
-
+class Cricketer{
+	String name="Kohli";
+	int age=30;
+	String color="white";
+	String role="batsman";
+	String national="Indian";
+	void Display()
+	{
+		System.out.println("I love cricket");
+	}
+		
+			
+}
 public class Br {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("main method started");
+Cricketer c = new Cricketer();
+c.Display();
+System.out.println("main method ended");
 	}
 
 }
