da4659a82d9ad7a667a82cea5c120e254e6cf37a
myusername <bharathinayak34@gmail.com>
2022-11-07 13:52:16 +0530

    first commit

---
+++ Printing/src/com/cg/printing/Printing.java
@@ -0,0 +1,10 @@
+package com.cg.printing;
+
+public class Printing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hlo world");
+	}
+
+}
---
+++ clas/src/clas/Clas.java
@@ -0,0 +1,10 @@
+package clas;
+
+public class Clas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		System.out.println("hello world..");
+	}
+
+}
