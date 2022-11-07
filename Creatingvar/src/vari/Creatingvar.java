293019d4c00b165b1bbc5323b176b2c78ddf6eb7
myusername <bharathinayak34@gmail.com>
2022-11-07 13:36:23 +0530

    first commit

---
+++ Creatingvar/src/vari/Creatingvar.java
@@ -0,0 +1,11 @@
+package vari;
+
+public class Creatingvar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ String name ="manu";
+ System.out.println(name);
+	}
+
+}
---
+++ Creatingvar/src/vari/casting.java
@@ -0,0 +1,12 @@
+package vari;
+
+public class casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int myInt=9;
+float mydouble=myInt;
+System.out.println(mydouble);
+	}
+
+}
