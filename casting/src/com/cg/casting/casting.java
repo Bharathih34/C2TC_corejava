d4347e0680ea5706e0197e47f53c259cc1a035ae
myusername <pramod013r@gmail.com>
2022-11-16 00:11:57 +0530

    first commit

---
+++ casting/src/com/cg/casting/casting.java
@@ -0,0 +1,15 @@
+package com.cg.casting;
+public class casting{
+public static void main(String[] args) {
+
+     int myInt = 9;
+
+     double myDouble = myInt;
+
+System.out.println(myDouble);
+
+System.out.println(myInt);
+
+}
+
+}
